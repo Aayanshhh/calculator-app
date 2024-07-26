5aa9ba34283902923822f08fba6c7680e0b7b19e
Aayansh Singh <shaktisinghkalhans@gmail.com>
2024-07-26 13:20:44 +0530

    changes for area in UI

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from calc_func import addition,subtraction
 from multiply import multiplication
+from area import calculate_area_rectangle
 def main():
     print("Welcome to the calculator app")
-    func = int(input("Select the function\n 1. Add\n 2. Subtract\n 3.Mutiply\n"))
+    func = int(input("Select the function\n 1. Add\n 2. Subtract\n 3.Mutiply\n 4.Area\n"))
     A = int(input("Input the value of A-:"))
     B = int(input("Input the value of B-:"))
     if func == 1:
         print(addition(A,B))
     elif func==2:
         print(subtraction(A,B))
-    
     elif func == 3:
         print(multiplication(A,B))
+    elif func == 4:
+        print(calculate_area_rectangle(A,B))
     
     
 if __name__ == '__main__':
